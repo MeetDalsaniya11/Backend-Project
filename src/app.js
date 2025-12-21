cc554d40ca51956ad2383692dc12c105d7f2a987
MeetDalsaniya11 <meet.dalsaniya1104@gmail.com>
2025-12-21 11:12:54 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    Credential : true
+}))
+
+app.use(express.json({limit:"16kb"}))  //getting json data
+app.use(express.urlencoded({extended : true , limit : "16kb"}))  //getting data from url
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,44 @@ import dotenv from "dotenv"
 dotenv.config({ path: "./env" });
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000 , () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import express from "express";
